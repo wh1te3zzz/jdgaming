f485aa33fe6825a315201bcb959e16406382e86e
wh1te3zzz <96282103+wh1te3zzz@users.noreply.github.com>
2022-01-17 15:07:03 +0800

    更新特物Z

--- jd_active_ProductZ.js
+++ jd_active_ProductZ.js
@@ -145,17 +145,17 @@ async function doTask(){
             if (signList.length === 0) {
                 console.log(`任务：${$.oneTask.assignmentName},信息异常`);
             }
-            if ($.oneTask.assignmentName.indexOf('首页下拉') !== -1) {
-                for (let j = 0; j < signList.length; j++) {
-                    if (signList[j].status === 1) {
-                        console.log(`任务：${$.oneTask.assignmentName},去执行,请稍稍`);
-                        let itemId = signList[j].itemId;
-                        $.runInfo = {'itemId':itemId};
-                        await takeRequest('superBrandDoTask');
-                        await $.wait(3000);
-                    }
+            //if ($.oneTask.assignmentName.indexOf('首页下拉') !== -1) {
+            for (let j = 0; j < signList.length; j++) {
+                if (signList[j].status === 1) {
+                    console.log(`任务：${$.oneTask.assignmentName},去执行,请稍稍`);
+                    let itemId = signList[j].itemId;
+                    $.runInfo = {'itemId':itemId};
+                    await takeRequest('superBrandDoTask');
+                    await $.wait(3000);
                 }
             }
+            //}
         }
     }
 }
