78ceab56da23b1fd70223cf2529f4ae82ba9f1bc
wh1te3zzz <96282103+wh1te3zzz@users.noreply.github.com>
2022-03-23 16:40:54 +0800

    京东极速版任务

--- jd_daily_JsTask.js
+++ jd_daily_JsTask.js
@@ -22,7 +22,6 @@ if ($.isNode()) {
 
 const JD_API_HOST = 'https://api.m.jd.com/', actCode = 'visa-card-001';
 
-
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
